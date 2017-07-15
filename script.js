478f7c5b8c837e0964956a309b0883617aacc709
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-07-15 08:59:28 +0100

    Added icon mapping

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ let get_weather = (data) => {
   let celsius = (data.currently.temperature - 32) /1.8
   $("#temperature").text(Math.floor(celsius) + " °C")
   $("#weather-summary").html(data.currently.summary)
+
+  let icon_mapping = {
+    "clear-day": "2",
+    "clear-night": "3",
+    "rain": "18",
+    "snow": "24",
+    "sleet": "23",
+    "wind": "6",
+    "fog": "13",
+    "cloudy": "14",
+    "partly-cloudy-day": "8",
+    "partly-cloudy-night": "9"
+  }
+
+  let icon_api = data.currently.icon
+  let icon_svg = icon_mapping[icon_api]
+
+  $('#weather-icon').append('<img src="/icons/6.svg">')
 }
 
 let get_location = (lat, lon) => {
